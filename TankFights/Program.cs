b172f7e044497f968725a73c4552a8c60f59d699
Владислав Труфанов <@local.itstep>
2019-03-06 14:09:33 +0600

    Добавьте файлы проекта.

---
+++ TankFights/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanks;
+
+namespace TankFights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tank> t34 = new List<Tank>();
+            List<Tank> panteras = new List<Tank>();
+            Tank winner = new Tank("");
+            int j = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                t34.Add(new Tank("T-34"));
+                panteras.Add(new Tank("Pantera"));
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Бой {0}", j++);
+                Console.ForegroundColor = ConsoleColor.White;
+                try
+                {
+                    if (t34[i] != null || panteras[i] != null)
+                    {
+                        Console.WriteLine(t34[i].getProperties());
+                        Console.WriteLine(panteras[i].getProperties());
+                        try
+                        {
+                            winner = t34[i] * panteras[i];
+                            if (winner != null)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Победил");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(winner.getProperties());
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ничья");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                }
+
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Танк не существует!!!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+    }
+}
---
+++ Tanks/Tanki.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    public class Tank
+    {
+        private string name { get; set; }
+        private double ammunitionLevel { get; set; }
+        private double armorLevel { get; set; }
+        private double manoeuvrability { get; set; }
+        private static Random rnd = new Random();
+
+
+        public Tank(string name)
+        {
+            this.name = name;
+            this.ammunitionLevel = rnd.Next(0, 100);
+            this.armorLevel = rnd.Next(0, 100);
+            this.manoeuvrability = rnd.Next(0, 100);
+        }
+
+        public string getProperties()
+        {
+            string result = "Имя: " + this.name + "\nБоекомплект: " + this.ammunitionLevel.ToString() + "\nУровень брони:"
+                + this.armorLevel.ToString() + "\nУровень маневренности: " + this.manoeuvrability.ToString()
+                + "\n**************************";
+            return result;
+        }
+
+        public static Tank operator *(Tank tank1, Tank tank2)
+        {
+            if ((tank1.ammunitionLevel > tank2.ammunitionLevel && tank1.armorLevel > tank2.armorLevel)
+                || (tank1.ammunitionLevel > tank2.ammunitionLevel && tank1.manoeuvrability > tank2.manoeuvrability)
+                    || (tank1.armorLevel > tank2.armorLevel && tank1.manoeuvrability > tank2.manoeuvrability))
+                return tank1;
+            else if ((tank1.ammunitionLevel < tank2.ammunitionLevel && tank1.armorLevel < tank2.armorLevel)
+                || (tank1.ammunitionLevel < tank2.ammunitionLevel && tank1.manoeuvrability < tank2.manoeuvrability)
+                    || (tank1.armorLevel < tank2.armorLevel && tank1.manoeuvrability < tank2.manoeuvrability))
+                return tank2;
+            else
+                return null;
+        }
+
+    }
+}
